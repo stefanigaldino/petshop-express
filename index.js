5029ce2d5ebdf3e0d935775c40fb997c888f18bd
Stefani Galdino <stefanijuris@hotmail.com>
2021-04-01 16:38:09 -0300

    GET, GETNOME, POST

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 // servidor e rotas
 const express = require('express');
+const petshop = require('./petshop');
+const {v4:uuidv4} = require('uuid');
+
 const app = express();
+app.use(express.json());
+
+//Rota para cada informação
+// app.get('/pets', (req, res) => {
+//     return res.send(petshop.listarPets());
+// })
+app.get('/pets/:nome',(req,res) => {
+    const { nome } = req.params;
+
+    //petshop.buscarPet(nome);
+    return res.send(petshop.buscarPet(nome));
+});
+
+app.post('/pets', (request, response) => {
+    const {tutor, contato, nome, tipo, idade, raca, peso, vacinado, servicos} = request.body;
+        
+    const pet = {nome, tipo, idade, raca, peso, tutor, contato, vacinado, servicos} 
+
+    petshop.adicionarPet(pet);
+
+    return response.send(pet);
+
+});
+
+
 
 app.listen(3000, () => {
     console.log('Servidor rodando!')
 });
+
+console.log(petshop.listarPets());
--- petshop.js
+++ petshop.js
@@ -1,9 +1,10 @@
-// modulo proprio
+// módulo próprio
 
 const moment = require('moment');
 const fs = require('fs');
+let bancoDados = fs.readFileSync('./bancoDados.json', 'utf-8');
 
-let bancoDados = JSON.parse(bancoDados);
+bancoDados = JSON.parse(bancoDados);
 
 const petshop = {
     atualizarBanco: () => {
@@ -11,14 +12,16 @@ const petshop = {
         fs.writeFileSync('bancoDados.json', petsAtualizado, 'utf-8');
     },
     listarPets: () => {
+        let textoListaPets = "PETSHOP \n";
 
         bancoDados.pets.forEach((pet) => {
-    
-            console.log(`${pet.nome}, ${pet.idade} anos, ${pet.tipo}, ${pet.raca}, ${(pet.vacinado) ? 'vacinado': 'não vacinado'}`);
+            textoListaPets += (`${pet.nome}, ${pet.idade} anos, ${pet.tipo}, ${pet.raca}, ${(pet.vacinado)
+            ? 'vacinado': 'não vacinado'} \n`);
             pet.servicos.forEach((servico) => {
-                console.log(`${servico.data} - ${servico.nome}`);
-            })
+                textoListaPets += (`${servico.data} - ${servico.nome} \n`);
+               });
         })
+        return textoListaPets;
     },
     vacinarPet: pet => {
         if (!pet.vacinado) {
@@ -52,7 +55,7 @@ const petshop = {
             bancoDados.pets.push(novoPet);
         })
     
-        atualizarBanco();
+        petshop.atualizarBanco();
         novosPets.forEach((pet) => {
             console.log(`${pet.nome} foi adicionado com sucesso!`);
         })
